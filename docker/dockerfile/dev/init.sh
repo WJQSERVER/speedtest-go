ff323f350efaedcd73e3880aad7e0520d9a04a11
WJQSERVER <wjqserver@outlook.com>
2025-01-03 22:44:23 +0800

    25w01c

--- docker/dockerfile/dev/init.sh
+++ docker/dockerfile/dev/init.sh
@@ -8,7 +8,7 @@ if [ ! -f /data/${APPLICATION}/config/config.toml ]; then
 fi
 
 # 启动 Go 应用
-/data/${APPLICATION}/${APPLICATION} > /data/${APPLICATION}/log/run.log 2>&1 &
+/data/${APPLICATION}/${APPLICATION} -cfg /data/${APPLICATION}/config/config.toml > /data/${APPLICATION}/log/run.log 2>&1 &
 
 # 保持脚本运行
 while true; do
